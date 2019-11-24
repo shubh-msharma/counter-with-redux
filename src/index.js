aa7968119fa5664e6124206be083b03a5517438c
shubh-msharma <shubhamsharma4539@gmail.com>
2019-11-24 20:32:20 +0530

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore , combineReducers}  from 'redux'
-import {  Provider  } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, combineReducers, applyMiddleware ,compose} from "redux";
+import { Provider } from "react-redux";
 
-import CounterReducer from './store/reducer/CounterReducer'
-import ResultReducer from './store/reducer/ResultReducer'
+import CounterReducer from "./store/reducer/CounterReducer";
+import ResultReducer from "./store/reducer/ResultReducer";
 
 const rootReducer = combineReducers({
-    ctrReducer:CounterReducer,
-    resReducer:ResultReducer
-})
+    ctrReducer: CounterReducer,
+    resReducer: ResultReducer
+});
 
-const store = createStore(rootReducer)
+function logger(store) {
+    return function (next) {
+        return function (action) {
+            console.log(next)
+            console.log(action);
+             next(action) 
+            // console.log(result)
+            // return result;
+        };
+    };
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
-ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 registerServiceWorker();
